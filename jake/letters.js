cdc257338abb6080fcfdb459daf065b98d97c192
Agnes Sliczniak <aga@iMac.local>
2016-05-06 01:22:14 -0500

    tried making the size never 0x0

--- jake/letters.js
+++ jake/letters.js
@@ -1,1 +1,10 @@
-function handle(c) { s.innerHTML = c; }
+function handle(c) {
+	t = c + "\n"
+	for (j = 1; j < h; j++) {
+		for (i = 0; i < w; i++)
+			t += "."
+		t += "\n"
+	}
+
+	s.innerHTML = t;
+}
